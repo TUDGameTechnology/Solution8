f6afc823d8195781809ad15ee4c77d8001a58da1
Polona Caserman <polona.caserman@gmail.com>
2017-12-11 17:59:45 +0100

    Update .gitignore

---
+++ Sources/ShaderProgram.h
@@ -0,0 +1,14 @@
+//
+//  ShaderProgram.hpp
+//  Exercise8
+//
+//  Created by Polona Caserman on 11.12.17.
+//  Copyright © 2017 KTX Software Development. All rights reserved.
+//
+
+#ifndef ShaderProgram_hpp
+#define ShaderProgram_hpp
+
+#include <stdio.h>
+
+#endif /* ShaderProgram_hpp */
